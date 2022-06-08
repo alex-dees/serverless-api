9c05b4a120c19415afb9cf09699de95e60842bc9
alex.dees@sparx.local <alex.dees@sparx.local>
2022-06-08 10:13:45 -0500

    initial commit

---
+++ lib/serverless-api-stack.ts
@@ -0,0 +1,11 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+
+export class ServerlessApiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const fn = new NodejsFunction(this, 'lambda');
+  }
+}
