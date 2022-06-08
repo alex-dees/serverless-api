b93c82f9e62f0cd3ee0fff55446fd9586e58c350
alex.dees <alex.dees@sparx.local>
2022-06-08 10:34:42 -0500

    add api event source

--- lib/serverless-api-stack.ts
+++ lib/serverless-api-stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { ApiEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
 export class ServerlessApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const fn = new NodejsFunction(this, 'lambda');
+    fn.addEventSource(new ApiEventSource('GET', '/items'));
   }
 }
